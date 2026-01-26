27663ce3ea88eb98a0001072be512c3f8af5f693
NamTheDev <thica.doi@gmail.com>
2026-01-26 10:26:15 +0700

    chore: update entry point for new initialization flow

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ const client = new Client({
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
     GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildMembers,
   ],
   partials: [Partials.Message, Partials.Channel, Partials.Reaction],
 });
