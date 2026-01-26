9cc3d238562854d628b51f06c9eff41fc2bc327d
NamTheDev <thica.doi@gmail.com>
2026-01-26 10:27:11 +0700

    fix: hydrate data caches on bot startup

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,10 +1,18 @@
 import { Client, Events } from "discord.js";
 import { BotEvent } from "../types";
+import { loadGiveaways, resumeGiveaways } from "../utils/giveaway";
+import { loadReactionRoles } from "../utils/reactionRoles";
 
 export const event: BotEvent = {
   name: Events.ClientReady,
   once: true,
-  execute(client: Client) {
+  execute: async (client: Client) => {
     console.log(`Ready! Logged in as ${client.user?.tag}`);
+
+    await loadGiveaways();
+    await loadReactionRoles();
+
+    const guild = client.guilds.cache.get(process.env.GUILD_ID!);
+    if (guild) resumeGiveaways(guild);
   },
 };
